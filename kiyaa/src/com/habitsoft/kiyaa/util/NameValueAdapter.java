6488f14cf210f1877d035959a943efd3bfa24892
dobesv <dobesv@1ac863c4-a705-11dd-8184-3158da6531e3>
2008-11-20 04:47:46 +0000

    Add a factory for string-only views to make those easier to create and use in custom subclasses of CustomPopup, CustomComboBox, and TableView.

--- kiyaa/src/com/habitsoft/kiyaa/util/NameValueAdapter.java
+++ kiyaa/src/com/habitsoft/kiyaa/util/NameValueAdapter.java
@@ -3,7 +3,23 @@
  */
 package com.habitsoft.kiyaa.util;
 
+/**
+ * Object used to adapt objects for use in a listbox, since listbox
+ * doesn't allow you store objects as their value, only strings.
+ */
 public interface NameValueAdapter<T> {
+    /**
+     * Return the label to use for the list item.  If the model is null,
+     * this should return a non-null string, such as "".
+     */
 	public String getName(T model);
+	
+	/**
+	 * Return the value to use for the list item.  If the model is null,
+	 * this should return a non-null string, such as "null".
+	 * 
+	 * Note that null listbox values work with some browsers but not others,
+	 * so just don't do it and you'll avoid some weirdness.
+	 */
 	public String getValue(T model);
 }
