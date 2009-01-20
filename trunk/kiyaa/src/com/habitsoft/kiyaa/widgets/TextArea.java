99a83a5e694db3e74aff4039fe385cebe5259c6a
dobesv <dobesv@1ac863c4-a705-11dd-8184-3158da6531e3>
2009-01-20 04:47:32 +0000

    maxLength support for textarea

--- trunk/kiyaa/src/com/habitsoft/kiyaa/widgets/TextArea.java
+++ trunk/kiyaa/src/com/habitsoft/kiyaa/widgets/TextArea.java
@@ -5,6 +5,7 @@ import com.habitsoft.kiyaa.util.FocusGroup;
 public class TextArea extends com.google.gwt.user.client.ui.TextArea {
 	String oldText;
     FocusGroup focusGroup;
+    int maxLength;
     
 	@Override
 	public void setText(String text) {
@@ -16,7 +17,10 @@ public class TextArea extends com.google.gwt.user.client.ui.TextArea {
 
 	@Override
 	public String getText() {
-	    return (oldText = super.getText());
+	    String text = super.getText();
+	    if(maxLength != 0 && text.length() > maxLength)
+	        text = text.substring(0, maxLength);
+        return (oldText = text);
 	}
     public void setFocusGroup(FocusGroup group) {
         if(this.focusGroup != null)
@@ -24,6 +28,24 @@ public class TextArea extends com.google.gwt.user.client.ui.TextArea {
         this.focusGroup = group;
         if(group != null)
             group.add(this);
+    }
+    
+    /**
+     * Gets the maximum allowable length of the text area.
+     * 
+     * @return the maximum length, in characters
+     */
+    public int getMaxLength() {
+      return maxLength;
+    }
+
+    /**
+     * Sets the maximum allowable length of the text area.
+     * 
+     * @param length the maximum length, in characters
+     */
+    public void setMaxLength(int length) {
+        maxLength = length;
     }
     
 }
