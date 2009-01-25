850c739695230311b146951b5705b251f007e1cc
dobesv <dobesv@1ac863c4-a705-11dd-8184-3158da6531e3>
2009-01-25 15:08:59 +0000

    When logging error markers, use gwt-log so it goes somewhere.

--- kiyaa/src/com/habitsoft/kiyaa/util/AsyncCallbackProxy.java
+++ kiyaa/src/com/habitsoft/kiyaa/util/AsyncCallbackProxy.java
@@ -1,18 +1,28 @@
 package com.habitsoft.kiyaa.util;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class AsyncCallbackProxy<T> implements AsyncCallback<T> {
 	protected final AsyncCallback callback;
-
-	public AsyncCallbackProxy(AsyncCallback delegate) {
+	protected final Object marker;
+	
+	public AsyncCallbackProxy(AsyncCallback delegate, Object marker) {
 		super();
 		//if(!(this instanceof AsyncCallbackWithTimeout) && !(delegate instanceof AsyncCallbackWithTimeout))
 		//	delegate = new AsyncCallbackWithTimeout<T>(delegate);
 		this.callback = delegate;
+		this.marker = marker;
+	}
+	
+	public AsyncCallbackProxy(AsyncCallback delegate) {
+	    this(delegate, null);
 	}
 
 	public void onFailure(Throwable caught) {
+	    if(marker != null && (caught instanceof Error) || (caught instanceof RuntimeException)) try {
+	        Log.error("At "+marker.toString(), caught);
+	    } catch(Exception e) { }
 		if(callback != null)
 			callback.onFailure(caught);
 	}
@@ -21,6 +31,10 @@ public class AsyncCallbackProxy<T> implements AsyncCallback<T> {
 		if(callback != null)
 			callback.onSuccess(result);
 	}
+
+    public Object getMarker() {
+        return marker;
+    }
 	
 
 }
