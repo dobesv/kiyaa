a4955554c69bde3a7b1d23bada492ce06d21be79
dobesv <dobesv@1ac863c4-a705-11dd-8184-3158da6531e3>
2009-02-19 01:39:09 +0000

    Handle parse errors in the percentage text box by returning a "default value".

--- kiyaa/src/com/habitsoft/kiyaa/widgets/PercentageTextBox.java
+++ kiyaa/src/com/habitsoft/kiyaa/widgets/PercentageTextBox.java
@@ -13,9 +13,16 @@ public class PercentageTextBox extends TextBox {
 	double cachedValue=0;
 	String cachedText="0%";
 	LocalizedParser parser = new ClientLocalizedParser();
+	double defaultValue=0;
 	
 	public double getDoubleValue() {
-	    return parser.parsePercentage(getText().trim());
+	    String text = getText().trim();
+	    if(text.length() == 0) return defaultValue;
+        try {
+            return parser.parsePercentage(text);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
 	}
 	public void setDoubleValue(double value) {
 		// Try to avoid expensive string operations
@@ -29,4 +36,10 @@ public class PercentageTextBox extends TextBox {
 		cachedText = text;
 		setText(text);
 	}
+    public double getDefaultValue() {
+        return defaultValue;
+    }
+    public void setDefaultValue(double defaultValue) {
+        this.defaultValue = defaultValue;
+    }
 }
