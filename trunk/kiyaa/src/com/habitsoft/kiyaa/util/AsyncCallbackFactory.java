64f592fb9f961e3773fa6bc69eb901c41559105f
dobesv <dobesv@1ac863c4-a705-11dd-8184-3158da6531e3>
2009-02-19 05:50:46 +0000

    Change AsyncCallbackFactory so it's less likely I'll implement it incorrectly.

--- trunk/kiyaa/src/com/habitsoft/kiyaa/util/AsyncCallbackFactory.java
+++ trunk/kiyaa/src/com/habitsoft/kiyaa/util/AsyncCallbackFactory.java
@@ -4,31 +4,41 @@ import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public class AsyncCallbackFactory {
+public abstract class AsyncCallbackFactory {
 	/**
 	 * Default implement will alert() if the operation fails.  Subclasses should override this
 	 * to do something more cross-browser friendly (IE blocks alerts these days).
 	 */
-	public AsyncCallback newInstance() {
-        return newInstance(null);
-	}
+	public abstract AsyncCallback newInstance();	
+	public AsyncCallback newInstance(final Object marker) { return newInstance(); }
 	
-	public AsyncCallback newInstance(final Object marker) {
-        return new AsyncCallback() {
-            public void onSuccess(Object arg0) {
-            }
-            public void onFailure(Throwable caught) {
-                if(marker != null && (caught instanceof Error || caught instanceof RuntimeException)) try {
-                    Log.error("At "+marker.toString(), caught);
-                } catch(Exception e) { }
-                if(caught.getMessage() != null)
-                    Window.alert(caught.getMessage());
-                else
-                    Window.alert(caught.toString());
-            }
-        };
-	}
-	static AsyncCallbackFactory defaultFactory = new AsyncCallbackFactory();
+	static AsyncCallbackFactory defaultFactory = new AsyncCallbackFactory() {
+	    /**
+	     * Default implement will alert() if the operation fails.  Subclasses should override this
+	     * to do something more cross-browser friendly (IE blocks alerts these days).
+	     */
+	    @Override
+        public AsyncCallback newInstance() {
+	        return newInstance(null);
+	    }
+	    
+	    @Override
+        public AsyncCallback newInstance(final Object marker) {
+	        return new AsyncCallback() {
+	            public void onSuccess(Object arg0) {
+	            }
+	            public void onFailure(Throwable caught) {
+	                if(marker != null && (caught instanceof Error || caught instanceof RuntimeException)) try {
+	                    Log.error("At "+marker.toString(), caught);
+	                } catch(Exception e) { }
+	                if(caught.getMessage() != null)
+	                    Window.alert(caught.getMessage());
+	                else
+	                    Window.alert(caught.toString());
+	            }
+	        };
+	    }
+	};
 
 	public static AsyncCallbackFactory getDefaultFactory() {
 		return defaultFactory;
