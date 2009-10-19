56a61eccc28613557a0c689083952f0ae0c1f132
dobesv@gmail.com <dobesv@gmail.com@1ac863c4-a705-11dd-8184-3158da6531e3>
2009-10-19 20:40:58 +0000

    Add pow10 method to MathUtil.

--- trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/util/MathUtil.java
+++ trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/util/MathUtil.java
@@ -33,14 +33,30 @@ public class MathUtil {
      * Half-integers will be rounded up.
      */
     public static long roundToFixedPoint(double val, int decimalPlaces) {
+    	return round(pow10(val, decimalPlaces));
+    }
+
+    /**
+     * Raise the number by the given power of 10.
+     * 
+     * roundTo(x, 0) == 0
+     * roundTo(4, 1) == 40
+     * roundTo(4.45, 1) == 44.5
+     * roundTo(-4.45, 1) == -4.45
+     * roundTo(-4.455, 2) == -4.455
+     * roundTo(4.455, 2) == 4.455
+     * 
+     * Supports either 0, 1, 2, 3, -1, -2, or -3 powers.
+     */
+    public static double pow10(double val, int decimalPlaces) {
         switch(decimalPlaces) {
-        case 0: return round(val);
-        case 1: return round(val*10.0);
-        case 2: return round(val*100.0);
-        case 3: return round(val*1000.0);
-        case -1: return round(val*0.1);
-        case -2: return round(val*0.01);
-        case -3: return round(val*0.001);
+        case 0: return (val);
+        case 1: return (val*10.0);
+        case 2: return (val*100.0);
+        case 3: return (val*1000.0);
+        case -1: return (val*0.1);
+        case -2: return (val*0.01);
+        case -3: return (val*0.001);
         default:
             throw new IllegalArgumentException("Unsupported number of decimal places: "+decimalPlaces);
         }
