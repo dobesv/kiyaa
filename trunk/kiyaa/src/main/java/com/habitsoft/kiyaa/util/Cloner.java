113977dc0adec4451036fed849ab853bbc094d36
dobesv@gmail.com <dobesv@gmail.com@1ac863c4-a705-11dd-8184-3158da6531e3>
2010-02-05 18:36:27 +0000

    Add support to Cloner for "diffing" two objects that could have been cloned.  This allows you to clone an object, make changes, and then get an idea of which fields were changed afterwards (if any).

--- trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/util/Cloner.java
+++ trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/util/Cloner.java
@@ -1,8 +1,41 @@
 package com.habitsoft.kiyaa.util;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public interface Cloner<T extends Serializable> {
 
+	/**
+	 * Create a new instance of T and then
+	 * use <code>clone(src, dest)</code> to copy the given
+	 * object into it.  Then return that object.
+	 */
 	public T clone(T src);
+	
+	/**
+	 * Copy all properties from src to dest.
+	 * 
+	 * <ul>
+	 * <li>Native arrays are deep-copied.</li>
+	 * <li>Objects with a <code>clone()</code> method will have that called</li>
+	 * <li>Everything else is copied as-is using the setter on the target object.</li>
+	 * </ul>
+	 */
+	public void clone(T src, T dest);
+	
+	/**
+	 * Compare the properties of a and b and return a list
+	 * of the names of the properties which differ.
+	 * 
+	 * <ul>
+	 * <li>Objects are compared using equals()</li>
+	 * <li>Native arrays are compared using Arrays.equals()</li>
+	 * <li>Primitive types are compared using ==</li>
+	 * </ul>
+	 * 
+	 * When two arrays differ, the elements are compared one at a time
+	 * and any changed, added, or removed element indexes are reported in
+	 * <arrayPropery>[<index>] format.
+	 */
+	public Set<String> diff(T a, T b);
 }
