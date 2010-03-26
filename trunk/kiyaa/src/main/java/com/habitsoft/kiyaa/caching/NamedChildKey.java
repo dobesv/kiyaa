6c6344382defe52755729eaf5a379168986d111a
dobesv@gmail.com <dobesv@gmail.com@1ac863c4-a705-11dd-8184-3158da6531e3>
2010-03-26 17:18:17 +0000

    Don't allow null parent id in NamedChildKey.

--- trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/caching/NamedChildKey.java
+++ trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/caching/NamedChildKey.java
@@ -3,12 +3,15 @@
  */
 package com.habitsoft.kiyaa.caching;
 
+import org.apache.commons.lang.NullArgumentException;
+
 
 public class NamedChildKey implements Comparable<NamedChildKey> {
 	final String childName;
 	final Long parentId;
 	public NamedChildKey(Long parent, String childName) {
 		super();
+		if(parent == null) throw new NullArgumentException("parent id must not be null");
 		this.parentId = parent;
 		this.childName = childName==null?"":childName;
 	}
