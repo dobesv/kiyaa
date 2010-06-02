7c818f171a645473a661c5c81a040f005c541418
dobesv@gmail.com <dobesv@gmail.com@1ac863c4-a705-11dd-8184-3158da6531e3>
2010-06-02 19:54:38 +0000

    Updated to AsyncCallbackShared so it can be used to check whether an operation is in progress and re-used multiple times.

--- trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/util/AsyncCallbackShared.java
+++ trunk/kiyaa/src/main/java/com/habitsoft/kiyaa/util/AsyncCallbackShared.java
@@ -17,7 +17,30 @@ public class AsyncCallbackShared<T> implements AsyncCallback<T> {
 	public AsyncCallbackShared(AsyncCallback<T> firstCallback) {
 		callbacks.add(firstCallback);
 	}
-	
+
+	/**
+	 * Construct the shared callback in an initially "done" state.  The
+	 * callback has to be reset in order to be used again.
+	 * 
+	 * Useful for cases where you want to use isDone() as a flag for
+	 * whether an operation is in progress or if the shared callback
+	 * is currently "idle".
+	 * 
+	 * @param result Result to return to any callback added
+	 */
+	public AsyncCallbackShared(T result) {
+		super();
+		this.done = true;
+		this.result = result;
+	}
+
+	/**
+	 * If the callback has already been called (isDone()) then
+	 * returns the success / failure it received before.
+	 * 
+	 * Otherwise the callback is stored and will be fired
+	 * when this callback is invoked with success / failure
+	 */
 	public void addCallback(AsyncCallback<T> callback) {
 		if(done) {
 			fireCallback(callback);
@@ -44,6 +67,14 @@ public class AsyncCallbackShared<T> implements AsyncCallback<T> {
 		this.done = true;
 		this.fireCallbacks();
 	}
+	
+	/**
+	 * Clear the "done" flag and any cached result from a prior call.
+	 * 
+	 * When done == false the callback could be considered to be "in use",
+	 * use reset() before starting a new operation to indicate that an
+	 * operation is in progress.
+	 */
 	public void reset() {
 	    callbacks.clear();
         result = null;
@@ -55,6 +86,27 @@ public class AsyncCallbackShared<T> implements AsyncCallback<T> {
 		this.result = result;
 		this.done = true;
 		this.fireCallbacks();
+	}
+
+	public ArrayList<AsyncCallback<T>> getCallbacks() {
+		return callbacks;
+	}
+
+	/**
+	 * True if this callback was invoked with success or failure.
+	 * 
+	 * Also true if the constructor was invoked with a result parameter.
+	 */
+	public boolean isDone() {
+		return done;
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+	public Throwable getCaught() {
+		return caught;
 	}
 
 }
