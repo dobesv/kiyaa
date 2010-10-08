551bcc6e2924ae5d2fec3db4f191499ebe8fe9ea
dobesv@gmail.com <dobesv@gmail.com@1ac863c4-a705-11dd-8184-3158da6531e3>
2010-10-08 19:54:04 +0000

    Change WebModeStackTrace to use the new StackTraceService instead of its old Javascript based algorithm.

--- kiyaa/src/main/java/com/habitsoft/kiyaa/util/WebModeStackTrace.java
+++ kiyaa/src/main/java/com/habitsoft/kiyaa/util/WebModeStackTrace.java
@@ -1,29 +1,45 @@
 package com.habitsoft.kiyaa.util;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsonUtils;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
 public class WebModeStackTrace {
 
-	public static native String getStack(Throwable e) /*-{
-		// Firefox
-		if(e.stack)
-			return e.stack;
-			
-		// Opera
-		if(window.opera && e.message)
-			return e.message;
-			
-		// Else IE or Safari
-	    var currentFunction = arguments.callee.caller;
-	    var callstack = 'stack trace:';
-	    while (currentFunction && callstack.length < 99) {
-	      var fn = currentFunction.toString();
-	      var fname = fn.substring(fn.indexOf("function") + 8, fn.indexOf("(")) || "anonymous";
-	      if(callstack.indexOf(fname) != -1) {
-	         callstack += '\n'+fname+' (mutually recursive calls - remaining stack trace unavailable)';
-	      }
-	      callstack += '\n'+fname;
-	      currentFunction = currentFunction.caller;
-	    }
-	    return callstack;
-	}-*/;
+	/**
+	 * Visible for testing.
+	 */
+	public static String getStack(Throwable t) {
+		StringBuilder sb = new StringBuilder();
+		boolean needsComma = false;
+		for (StackTraceElement e : t.getStackTrace()) {
+			if (needsComma) {
+				sb.append("\n");
+			} else {
+				needsComma = true;
+			}
+
+			sb.append(JsonUtils.escapeValue(e.getMethodName()));
+		}
+		return sb.toString();
+	}
+	
+	public static void log(String serviceUrl, String loggerName, Throwable t) {
+		if(GWT.isScript()) {
+			StackTraceServiceAsync service = GWT.create(StackTraceService.class);
+			((ServiceDefTarget)service).setServiceEntryPoint(serviceUrl);
+			service.log(GWT.getModuleName(), GWT.getPermutationStrongName(), loggerName, t.getClass().getName(), t.getMessage(), t.getStackTrace(), new AsyncCallback<Void>() {
+				@Override
+				public void onFailure(Throwable caught) {
+				}
+				@Override
+				public void onSuccess(Void result) {
+				}
+			});
+		} else {
+			t.printStackTrace();
+		}
+	}
 }
+
